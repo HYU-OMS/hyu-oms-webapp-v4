bf2fb385148647bb486948b986c8945157b76d55
Kihoon Han <khhan1993@gmail.com>
2019-07-02 22:45:43 +0900

    기본 구조 잡는 중, subdirectory 에 react project 생성

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,9 +1,22 @@
 import * as Koa from 'koa';
+import * as KoaRouter from 'koa-router';
+import * as KoaStatic from 'koa-static';
 import * as http from 'http';
+import * as path from 'path';
 
 const app: Koa = new Koa();
+const router: KoaRouter = new KoaRouter();
 
-// TODO: 나머지 코드를 작성
+/*
+ * 이 부분은 Static File 을 serve 하는 코드임.
+ * NODE_ENV 에 따라 Static file serve 방식을 다르게 함
+ */
+if(process.env.NODE_ENV === 'development') {
+
+}
+else {
+  app.use(KoaStatic(path.join(__dirname, '../', 'react-app', 'build')));
+}
 
 /* HTTP 서버를 생성 후 export */
 const server: http.Server = http.createServer(app.callback());
