81711fd7afb6239a6fe7944140a65eedb9098eca
Kihoon Han <khhan1993@gmail.com>
2019-07-03 22:55:53 +0900

    Koa -> Express 변경

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,23 +1,27 @@
-import * as Koa from 'koa';
-import * as KoaRouter from 'koa-router';
-import * as KoaStatic from 'koa-static';
+import * as express from 'express';
+import * as httpProxy from 'http-proxy';
 import * as http from 'http';
 import * as path from 'path';
 
-const app: Koa = new Koa();
-const router: KoaRouter = new KoaRouter();
+const app = express();
+
+/* "/api" 로 들어오는 모든 요청은 아래 handler 가 처리하게 됨. */
+// TODO: 관련 코드 작성
 
 /*
  * 이 부분은 Static File 을 serve 하는 코드임.
  * NODE_ENV 에 따라 Static file serve 방식을 다르게 함
  */
-if(process.env.NODE_ENV === 'development') {
-
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../', 'react-app', 'build')));
 }
 else {
-  app.use(KoaStatic(path.join(__dirname, '../', 'react-app', 'build')));
+  const proxy: httpProxy = httpProxy.createProxyServer({});
+  app.use(async (req, res) => {
+    proxy.web(req, res, { target: 'http://localhost:8899' });
+  });
 }
 
 /* HTTP 서버를 생성 후 export */
-const server: http.Server = http.createServer(app.callback());
+const server: http.Server = http.createServer(app);
 export default server;
