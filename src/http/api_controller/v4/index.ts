cf53e5eb4c84c509252d8c656b9f466bea43b61a
Kihoon Han <khhan1993@gmail.com>
2019-07-04 22:27:14 +0900

    JsonWebToken 관련 코드 작성 중

--- src/http/api_controller/v4/index.ts
+++ src/http/api_controller/v4/index.ts
@@ -12,6 +12,31 @@ const app: Application = asyncify(express());
 /* 기본 설정 */
 app.use(helmet());
 
+/* JWT 가 Authorization header 또는 URL query 'jwt' 에 존재하는지 확인 */
+app.use(async (req: Request, res: Response, next: any) => {
+  if(Boolean(req.get('Authorization')) === true || Boolean(req.query['jwt']) === true) {
+    let token: string = undefined;
+
+    if (Boolean(req.get('Authorization')) === true) {
+      const authorization: Array<string> = req.get('Authorization').split(' ');
+      if (authorization[0] !== 'Bearer' || authorization.length !== 2) {
+        throw createError(400, "'Authorization' must be 'Bearer [token]'.", {
+          state: 'AUTH_HEADER_FORMAT_ERR',
+          info: ['Authorization']
+        });
+      }
+
+      token = authorization[1];
+    } else {
+      token = req.query['jwt'];
+    }
+
+    // TODO: token decode 코드를 작성한다.
+  }
+
+  next();
+});
+
 // TODO: 여기에 각 route 에 해당하는 controller 를 연결하게 된다.
 
 /* 서버 Alive 체크를 위한 것 */
