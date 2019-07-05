7473b92aace49a7a8a5879efeceb2ac9b2f8d970
Kihoon Han <khhan1993@gmail.com>
2019-07-05 13:54:12 +0900

    config 파일 내 key 에 접근을 하지 못하는 문제로 인해 일단 any type 으로 변경

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,13 @@
  * Production 환경에서는 반드시 Environment Variable 을 통해 적절한 값을 지정하시기 바랍니다.
  */
 
-const v4: object = {
-  'jwt': {
-    'secret_key': (process.env.HYU_OMS_JWT_SECRET_KEY || 'uEbYw@y3Lh+hSa33'),
-    'algorithm': (process.env.HYU_OMS__JWT_ALGORITHM || 'HS512')
+const config: any = {
+  'v4': {
+    'jwt': {
+      'secret_key': (process.env.HYU_OMS_JWT_SECRET_KEY || 'uEbYw@y3Lh+hSa33'),
+      'algorithm': (process.env.HYU_OMS__JWT_ALGORITHM || 'HS512')
+    }
   }
 };
 
-export {
-  v4
-};
+export default config;
