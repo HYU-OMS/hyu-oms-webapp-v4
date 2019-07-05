3a1e29966874a0932ee9410b5e8e8f7c4e0f8cd3
Kihoon Han <khhan1993@gmail.com>
2019-07-05 13:11:16 +0900

    frontend 파일 serve 관련 -> 요청한 파일이 없을 경우 기본값으로 index.html 을 보내주도록 수정

--- src/http/index.ts
+++ src/http/index.ts
@@ -32,7 +32,13 @@ app.use('/api', api_controller);
  * NODE_ENV 에 따라 Static file serve 방식을 다르게 함
  */
 if(process.env.NODE_ENV === 'production') {
+  /* 기본적으로 요청한 file 을 전송한다. */
   app.use(express.static(path.join(__dirname, '../', 'react-app', 'build')));
+
+  /* 다만, 요청한 파일이 없을 경우 기본값으로 index.html 을 보내준다. (React 에서 URI route 관련해서 나머지를 처리한다) */
+  app.use('*', async (req: CustomRequest, res: CustomResponse) => {
+    res.sendFile(path.join(__dirname, '../', 'react-app', 'build', 'index.html'));
+  });
 }
 else {
   const proxy: httpProxy = httpProxy.createProxyServer({});
