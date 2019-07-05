77ad3d08cf8b56a863d08895f8010693c15e0726
Kihoon Han <khhan1993@gmail.com>
2019-07-05 22:29:02 +0900

    이전 버전의 WebApp code 가져오는 중.
    이전 버전 링크: https://github.com/HYU-OMS/hyu-oms-webapp-v3

--- src/react-app/src/index.tsx
+++ src/react-app/src/index.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { SnackbarProvider } from 'notistack';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//const store = createStore(reducer);
+const max_snack = (window.innerWidth < 600) ? 1 : 5;
+const horizontal_position = (window.innerWidth < 600) ? 'center': 'left';
+
+ReactDOM.render(
+  //<Provider store={store}>
+    <BrowserRouter>
+      <SnackbarProvider
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: horizontal_position,
+        }}
+        maxSnack={max_snack}
+        dense={window.innerWidth < 600}
+      >
+
+      </SnackbarProvider>
+    </BrowserRouter>
+  //</Provider>
+  ,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
