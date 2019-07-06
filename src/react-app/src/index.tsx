2a1619ce053436d30538d6ddbca011f620fac6d8
Kihoon Han <khhan1993@gmail.com>
2019-07-06 15:33:46 +0900

    notistack 제거

--- src/react-app/src/index.tsx
+++ src/react-app/src/index.tsx
@@ -3,33 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { SnackbarProvider } from 'notistack';
 
 import reducer from './reducer';
-
-import * as serviceWorker from './serviceWorker';
-
 import App from './component/App';
+import * as serviceWorker from './serviceWorker';
 
 import 'typeface-roboto';
 
 const store = createStore(reducer);
-const max_snack = (window.innerWidth < 600) ? 1 : 5;
-const horizontal_position = (window.innerWidth < 600) ? 'center': 'left';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <SnackbarProvider
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: horizontal_position,
-        }}
-        maxSnack={max_snack}
-        dense={window.innerWidth < 600}
-      >
-        <App />
-      </SnackbarProvider>
+      <App />
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
